7eab8ed31d509534f04fe3dee2c553a2496ba763
Frank Gundermann <clients@fogwebdesigns.com>
2017-05-02 14:34:51 -0700

    upgrade to     @angular/core: 2.3.1     rxjs: 5.0.0-rc.4     @angular/cli: 1.0.1
    move app.module

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,6 @@
-import './polyfills.ts';
+import './polyfills';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
